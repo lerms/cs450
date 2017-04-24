eb8ada5b32446dd76d216d0fb945a255225c4951
Alexander Lerma <alexanderrlerma@gmail.com>
2017-04-24 16:32:11 -0500

    added

---
+++ projects/pa4/src/xv6/sysproc.c
@@ -0,0 +1,152 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return proc->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = proc->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(proc->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int sys_callcount(void) {
+  cprintf("Alexander_Lerma_A20315236\n");
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  return proc->callcount[n];
+}
+
+static int is_valid_pde(uint *flags) {
+    return (PTE_P | PTE_W | PTE_U) & *flags;
+}
+
+static void check_flags(uint *present, uint *writable, uint *user, uint *ptef) {
+  //check each individual flag on each page table entry
+  if(PTE_P & *ptef)
+    (*present)++;
+  if(PTE_W & *ptef)
+    (*writable)++;
+  if(PTE_U & *ptef)
+    (*user)++;
+}
+
+//iterate over page table entries, check each of their flags.
+int sys_memcount(void) {
+  uint pages = PGROUNDUP(proc->sz) / PGSIZE;
+  uint present = 0, writable = 0, user = 0;
+  uint pdef, ptef;
+  pde_t *pde, *pte;
+
+  int i, j; 
+  for (i = 0; i < NPDENTRIES; i++) {
+    pde = &proc->pgdir[i];
+    pdef = PTE_FLAGS(*pde);
+
+    //check if page directory entry is valid
+    if(is_valid_pde(&pdef)) {
+      for(j = 0; j < NPTENTRIES; j++) {
+        pte = &pde[j];
+        ptef = PTE_FLAGS(*pte);
+        check_flags(&present, &writable, &user, &ptef);
+      }
+    }
+  }
+
+  cprintf("\n");
+  cprintf("********************************************\n");
+  cprintf("*************** Memory Count ***************\n");
+  cprintf("********************************************\n");
+  cprintf(" - Pages used by current process: %d\n", pages);
+  cprintf(" - Pages free: %d\n", kfreepagecount());
+  cprintf(" - Pages present: %d\n", present);
+  cprintf(" - Pages writable: %d\n", writable);
+  cprintf(" - Pages user accessible: %d\n", user);
+  cprintf("********************************************\n");
+  cprintf("********************************************\n");
+  cprintf("********************************************\n");
+  cprintf("\n");
+
+  return 0;
+}
